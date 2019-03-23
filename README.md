8a69918371b364eb8d3fe9a08a89b8256fcfc74c
Joshua Yuan <joshua43210@gmail.com>
2019-03-22 22:31:28 -0700

    add tqdm progress bar, and temporary file set up

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## TODO ##
-- delete ts files
-- locally store previous files
+- delete ts and temporary files
 - implement ffmpeg in code
 - support for windows and mac
 - GUI
 - just take url of page, and search network ourselves
-- throw it all into an web api
+- throw it all into an web tool
--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+from tqdm import tqdm
 
 class Game:
     def __init__(self, p, n, u, s):
@@ -9,9 +10,10 @@ class Game:
         self.url = u
         self.segments = s
         self.full_path = self.path + "/" + self.name
+        self.temp_path = self.full_path + "_TEMP" # temporary folder
 
 def create():
-    file_name= raw_input("Folder and file name (eg: pres-cal-col): ")
+    file_name= raw_input("File name (don't include the extension): ")
     # FILL THIS WITH YOUR OWN DEFAULT PATH
     path = os.path.abspath("")
     while True:
@@ -24,8 +26,8 @@ def create():
             break
         else:
             print("Please enter a valid path.")
-    print("Path to folder will be \'" + path + "/" + file_name +"\'")
-    url = raw_input("Paste url without the ###.ts: ")
+    print("Path to file will be \'" + path + "/" + file_name +".ts\'")
+    url = raw_input("Paste url without the '###.ts': ")
     segments = 0
     while True:
         segments = raw_input("Number of segments: ")
@@ -33,28 +35,30 @@ def create():
             segments = int(segments)
             break
         else:
-            print("Not a number.")
+            print("Not a valid number.")
     return Game(path, file_name, url, segments)
 
 
 def get_requests(game):
-    path = game.full_path
-    if not os.path.exists(path):
-        print ("\'" + path + "\' does not currently exist.")
-        print("Creating now...")
+    path = game.temp_path
+    if os.path.exists(path):
+        print ("\'" + path + "\' currently exist.")
+        return
+    else:
+        print ("Creating \'" + path + "\'")
         os.mkdir(path)
     request_pre_str = game.url
     request_post_str = ".ts"
-    for i in range(1, game.segments + 1):
+    for i in tqdm(range(1, game.segments + 1)):
         request_str = request_pre_str + str(i) + request_post_str
         download_video(request_str, path, str(i) + ".ts")
     print("Done downloading.")
 
 def download_video(request_str, save_to_dir, filename):
     request = requests.get(request_str, stream=True)
-    if request.status_code == 200:
-        print("Downloading clip", filename)
-    else:
+    if request.status_code != 200:
+        # print("Downloading clip", filename)
+    # else :
         print("Failed. Status_code=" + request.status_code)
         return
     with open(os.path.join(save_to_dir, filename), "wb") as f:
@@ -63,23 +67,24 @@ def download_video(request_str, save_to_dir, filename):
             if chunk:
                 f.write(chunk)
                 f.flush()
-        print("Done with", filename)
+        # print("Done with", filename)
 
 def merge_ts(path, num_files):
-    for i in range(1, num_files + 1):
+    for i in tqdm(range(1, num_files + 1)):
         with open(os.path.join(path, "full.ts"), "ab") as outfile:
             with open(os.path.join(path, str(i) + ".ts"), "rb") as infile:
                 outfile.write(infile.read())
-                print("Done reading from file", str(i) + ".ts")
+                # print("Done reading from file", str(i) + ".ts")
     print("Done merging ts")
 
 game = create()
 get_requests(game)
-merge_ts(game.full_path, game.segments)
+merge_ts(game.temp_path, game.segments)
 print("=======DONE=======")
 print("Run the following two commands.")
-print("cd " + game.full_path)
+print("cd " + game.temp_path)
 print("ffmpeg -i full.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc " + game.name + ".mp4")
 print("OR")
-print("ffmpeg -i " + game.full_path + "/full.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc " + game.full_path + "/" + game.name + ".mp4")
+print("ffmpeg -i " + game.temp_path + "/full.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc " + game.full_path + ".mp4")
+print("rm -rf " + game.temp_path)
 # ffmpeg -i INPUT.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc OUTPUT.mp4
