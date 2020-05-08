55875e3f0a9c93aabef403c8315dad0286ab479d
Joshua Yuan <joshua43210@gmail.com>
2020-05-08 00:21:37 -0700

    improved versatility

--- script.py
+++ script.py
@@ -3,21 +3,23 @@ import requests
 import json
 from tqdm import tqdm
 
-class Game:
-    def __init__(self, p, n, u, s):
+class Video:
+    def __init__(self, p, n, up, um, us, s):
         self.path = p
         self.name = n
-        self.url = u
+        self.url_prefix = up
+        self.url_mid= um
+        self.url_suffix = us
         self.segments = s
         self.full_path = self.path + "/" + self.name
         self.temp_path = self.full_path + "_TEMP" # temporary folder
 
 def create():
-    file_name= raw_input("File name (don't include the extension): ")
+    file_name= input("File name (don't include the extension): ")
     # FILL THIS WITH YOUR OWN DEFAULT PATH
     path = os.path.abspath("")
     while True:
-        new_path = raw_input("Path (default is \'" + path + "\'): ")
+        new_path = input("Path (default is \'" + path + "\'): ")
         if new_path == "":
             print("Sticking with default path.")
             break
@@ -27,29 +29,31 @@ def create():
         else:
             print("Please enter a valid path.")
     print("Path to file will be \'" + path + "/" + file_name +".ts\'")
-    url = raw_input("Paste url without the '###.ts': ")
+    url_prefix = input("Paste url prefix without '###.ts': ")
+    url_mid = input("Paste anything between the ## and .ts: ")
+    url_suffix = input("Paste url suffix: ")
     segments = 0
     while True:
-        segments = raw_input("Number of segments: ")
+        segments = input("Number of segments: ")
         if segments.isdigit() and int(segments) > 0:
             segments = int(segments)
             break
         else:
             print("Not a valid number.")
-    return Game(path, file_name, url, segments)
+    return Video(path, file_name, url_prefix, url_mid, url_suffix, segments)
 
 
-def get_requests(game):
-    path = game.temp_path
+def get_requests(video):
+    path = video.temp_path
     if os.path.exists(path):
         print ("\'" + path + "\' currently exist.")
         return
     else:
         print ("Creating \'" + path + "\'")
         os.mkdir(path)
-    request_pre_str = game.url
-    request_post_str = ".ts"
-    for i in tqdm(range(1, game.segments + 1)):
+    request_pre_str = video.url_prefix
+    request_post_str = video.url_mid + ".ts" + video.url_suffix
+    for i in tqdm(range(1, video.segments + 1)):
         request_str = request_pre_str + str(i) + request_post_str
         download_video(request_str, path, str(i) + ".ts")
     print("Done downloading.")
@@ -77,14 +81,14 @@ def merge_ts(path, num_files):
                 # print("Done reading from file", str(i) + ".ts")
     print("Done merging ts")
 
-game = create()
-get_requests(game)
-merge_ts(game.temp_path, game.segments)
+video = create()
+get_requests(video)
+merge_ts(video.temp_path, video.segments)
 print("=======DONE=======")
 print("Run the following two commands.")
-print("cd " + game.temp_path)
-print("ffmpeg -i full.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc " + game.name + ".mp4")
+print("cd " + video.temp_path)
+print("ffmpeg -i full.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc " + video.name + ".mp4")
 print("OR")
-print("ffmpeg -i " + game.temp_path + "/full.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc " + game.full_path + ".mp4")
-print("rm -rf " + game.temp_path)
+print("ffmpeg -i " + video.temp_path + "/full.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc " + video.full_path + ".mp4")
+print("rm -rf " + video.temp_path)
 # ffmpeg -i INPUT.ts -acodec copy -vcodec copy -bsf:a aac_adtstoasc OUTPUT.mp4
